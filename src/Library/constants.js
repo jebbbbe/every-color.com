44c9351dbc7e504662d619f1644db88cf8070da7
jebbbbe <74994218+jebbbbe@users.noreply.github.com>
2025-02-18 23:31:40 -0500

    UI for color FOrmat

--- src/Library/constants.js
+++ src/Library/constants.js
@@ -23,3 +23,36 @@ export const colorBlindTypes = {
     "monochromacy":"Monochromacy",
 }
 
+
+
+export const colorFormats = {
+    "hexidecimalNumber":"Hexidecimal Number", // 0xff00ff
+    "hexidecimalString":"hexidecimal String",  // #ff00ff
+    "rgbString":"RGB String",                 //rgb(255, 87, 51)
+    "rgbInt":"RGBint",                        // [255,0,255]
+    "rgbfloat":"RGBfloat",                    // [1.0,0.123213,1.0]
+    "rgbVector":"RGBVector",                  // vec3(1.0,0.123213,1.0);
+    "hsl":"HSL",                              //hsl(14, 100%, 60%)
+    "hsv":"HSV",                              //hsv(14, 80%, 100%)
+    "hsb":"HSB",                              //hsb(14, 80%, 100%) // same format as above
+
+    // more strange ones: 
+    // CIEXYZ
+    // CIELAB (Lab)
+    // LCH (Lightness, Chroma, Hue) / HCL
+    // HWB (Hue, Whiteness, Blackness)
+    // YUV
+    // YIQ
+    // Color Temperature (Kelvin)
+    // Pantone/Spot Colors
+}
+// all those formats have weird alpha variations
+export const colorAlphaFormats = {
+    "hexidecimalAlphaNumber":"Hexidecimal Alpha Number", // 0xff00ffff
+    "hexidecimalAlphaString":"hexidecimal Alpha String",  // #ff00ffff
+    "rgbaString":"RGBA String",//rgba(255, 87, 51, 0.8)
+    "rgbaInt":"RGBAint", // [255,0,255,255]
+    "rgbafloat":"RGBAfloat",// [1.0,0.123213,1.0, 1.0]
+    "rgbaVector":"RGBAvector",// vec3(1.0,0.123213,1.0, 1.0);
+    "hsla":"HSLA"//hsla(14, 100%, 60%, 0.8)
+}
--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import { noScrollCamera } from "/Library/noScrollCamera.js"
 import { noScrollBar } from "/Library/noScrollBar.js"
 import { htmlScene } from "/Library/htmlScene.js"
-import { constants,hashTypes,colorBlindTypes } from "/Library/constants.js"
+import { constants,hashTypes,colorBlindTypes,colorFormats } from "/Library/constants.js"
 import { DynamicDropdown } from "/Library/dom/dropdown.js"
 import { setupModalEventListeners } from "/Library/dom/modal.js"
 
@@ -10,6 +10,7 @@ const rowClass = "row-thing"
 const scrollFactor = 8
 const defaultHash = hashTypes.gradientIV
 const defaultColorBlind = colorBlindTypes.none
+const defaultColorFormat = colorFormats.hexidecimalString
 const camera = new noScrollCamera(mainClass,rowClass,scrollFactor,constants.start)
 const scene = new htmlScene(mainClass,defaultHash)
 const scrollbar = new noScrollBar(constants.start/constants.absoluteMax);
@@ -19,8 +20,10 @@ camera.resize()
 scene.updateColors(camera.position)
 const dropdown0 = new DynamicDropdown("hashSelect",hashTypes,hashSelectUpdate,defaultHash)
 const dropdown1 = new DynamicDropdown("colorBlindSelect",colorBlindTypes, colorSelectUpdate   ,defaultColorBlind)
+const dropdown2 = new DynamicDropdown("colorFormat",colorFormats, console.log   ,defaultColorFormat)
 setupModalEventListeners()
 removeLoader()
+
 
 //setupfunctions
 function removeLoader(){
