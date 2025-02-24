1ad3ad96b5752fc7869badb276b3d98261ac03cc
jebbbbe <74994218+jebbbbe@users.noreply.github.com>
2025-02-23 19:03:29 -0500

    vercel build changes

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ export default defineConfig({
     root: "src", // Set the root directory to `src`
     publicDir: "../public", // Point to the public directory relative to the project root
     build: {
-        outDir: "../build" // Ensure the output is still in `dist`
+        outDir: "../build", // Ensure the output is still in `dist`
+        emptyOutDir: true,
     }
 });
